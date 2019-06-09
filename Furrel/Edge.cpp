863d2376a8957168e26361a13150e13ddcadc1df
Jonathan Vice <jajvice@gmail.com>
2019-06-09 19:32:32 +0200

    Update Edge documentation

--- Furrel/Edge.cpp
+++ Furrel/Edge.cpp
@@ -1,7 +1,14 @@
+// Copyright (c) 2019 Jonathan Vice
+// This file is part of Furrel.
+
 #include <iostream>
 #include "Edge.h"
 
+/* Public Member Functions */
+
 Edge::Edge(char readC, char writeC, bool dirC) : read(readC), write(writeC), dir(dirC) {}
+
+/* Getter Functions */
 
 char Edge::getRead() { return char(read); }
 
--- Furrel/Edge.h
+++ Furrel/Edge.h
@@ -1,3 +1,6 @@
+// Copyright (c) 2019 Jonathan Vice
+// This file is part of Furrel.
+
 #ifndef EDGE_H
 #define EDGE_H
 
@@ -5,13 +8,31 @@
 
 class Edge {
 public:
+	/* Default Constructor
+	
+	@param readC   Character to be read when traversing
+	@param writeC	 Character to be written when traversing
+	@param dirC    Bool to represent direction to take while traversing: true for left, false for right
+	
+	*/
 	Edge(char readC, char writeC, bool dirC);
+
+	/* Returns character to be read while traversing */
 	char getRead();
+
+	/* Returns character to be written while traversing */
 	char getWrite();
+
+	/* Returns direction to take while traversing */
 	bool getDirection();
 private:
+	/* Character to be read while traversing */
 	char read;
+
+	/* Character to be written while traversing */
 	char write;
+
+	/* Bool to represent direction to take while traversing */
 	bool dir;
 };
 
