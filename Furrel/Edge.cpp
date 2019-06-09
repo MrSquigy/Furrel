e0d17a49e83dc789c17d8bc85b861080e87c2b5e
Jonathan Vice <jajvice@gmail.com>
2019-06-09 16:49:58 +0200

    Convert raw pointers into smart pointers

--- Furrel/Edge.cpp
+++ Furrel/Edge.cpp
@@ -1,10 +1,9 @@
+#include <iostream>
 #include "Edge.h"
 
 Edge::Edge(char readC, char writeC, bool dirC) : read(readC), write(writeC), dir(dirC) {}
 
-bool Edge::checkCross(char letter) { return letter == read; }
-
-char Edge::getRead() { return read; }
+char Edge::getRead() { return char(read); }
 
 char Edge::getWrite() { return write; }
 
--- Furrel/Edge.h
+++ Furrel/Edge.h
@@ -1,10 +1,11 @@
 #ifndef EDGE_H
 #define EDGE_H
 
+#include <vector>
+
 class Edge {
 public:
-	Edge(char read, char write, bool dir);
-	bool checkCross(char letter);
+	Edge(char readC, char writeC, bool dirC);
 	char getRead();
 	char getWrite();
 	bool getDirection();
